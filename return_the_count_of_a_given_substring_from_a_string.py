5de3e5d88bc884d0cc96ad265fb0e280f7c797db
James Mikko P. Recario <jamesmikkoprecario@iskolarngbayan.pup.edu.ph>
2024-01-26 02:05:37 +0800

    Added the count method to count Emma in string

--- return_the_count_of_a_given_substring_from_a_string.py
+++ return_the_count_of_a_given_substring_from_a_string.py
@@ -2,4 +2,9 @@
 # Write a program to find how many times substring “Emma” appears in the given string.
 
 # Count string using count method
+
+given_string = "Emma is good developer. Emma is a writer."
+print(given_string)
+count_of_emma = given_string.count("Emma")
+
 # Print the count
